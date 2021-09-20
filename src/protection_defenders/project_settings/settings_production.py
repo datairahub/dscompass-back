4d0813b6198c99ff1c22c0df0ad73de9254a2ac2
Santiago Espinosa <saigesp@gmail.com>
2021-09-20 17:32:01 +0200

    Update settings_production.py

--- src/protection_defenders/project_settings/settings_production.py
+++ src/protection_defenders/project_settings/settings_production.py
@@ -38,8 +38,8 @@ SIMPLE_JWT = {
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = os.environ.get('DJANGO_EMAIL_PORT')
+EMAIL_HOST = os.environ.get('DJANGO_EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
